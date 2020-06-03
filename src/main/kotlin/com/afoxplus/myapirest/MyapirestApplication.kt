367dbd9d2e6ac64934b7c90c761a1d262a43b76c
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2020-06-03 01:28:23 -0500

    initial commit

---
+++ src/main/kotlin/com/afoxplus/myapirest/MyapirestApplication.kt
@@ -0,0 +1,11 @@
+package com.afoxplus.myapirest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyapirestApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyapirestApplication>(*args)
+}
---
+++ src/test/kotlin/com/afoxplus/myapirest/MyapirestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.afoxplus.myapirest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyapirestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
